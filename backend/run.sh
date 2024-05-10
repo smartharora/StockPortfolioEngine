0d90a2c559543ed02dda8ba011d5b4e0555f2dc9
Smarth Arora <smartharora77@gmail.com>
2024-05-10 13:03:23 -0700

    Changes for deploying it on Vercal

--- backend/run.sh
+++ backend/run.sh
@@ -5,8 +5,8 @@ source venv/bin/activate
 
 # set api key
 export PORT=8000
-export STOCK_API_URL=https://api.iex.cloud
-export STOCK_API_KEY=
+#export STOCK_API_URL=https://api.iex.cloud
+#export STOCK_API_KEY=
 
 # Run Flask app
 flask run --host 0.0.0.0 --port 8000 --debugger
--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -3,7 +3,8 @@ import axios from "axios";
 // const API_BASE_URL = process.env.REACT_APP_BACKEND_API_URL || "http://127.0.0.1:8000";
 // const STOCK_API_KEY = process.env.REACT_APP_STOCK_API_KEY;
 
-const API_BASE_URL = "https://saiteja377.pythonanywhere.com/";
+const API_BASE_URL = "https://smartharora.pythonanywhere.com/";
+
 
 const API = axios.create({
     baseURL: API_BASE_URL,
